0db8ba070e96d820dca752e028188007b3ebf7ed
cogu <congus8@gmail.com>
2019-10-03 22:31:57 +0200

    Fix some issues with unpack macros

--- test/testsuite_pack.c
+++ test/testsuite_pack.c
@@ -24,6 +24,7 @@
 // LOCAL FUNCTION PROTOTYPES
 //////////////////////////////////////////////////////////////////////////////
 static void test_pack_unpack_LE64(CuTest* tc);
+static void test_unpack_little_endian_using_macros(CuTest* tc);
 
 //////////////////////////////////////////////////////////////////////////////
 // GLOBAL VARIABLES
@@ -44,6 +45,7 @@ CuSuite* testsuite_pack(void)
    CuSuite* suite = CuSuiteNew();
 
    SUITE_ADD_TEST(suite, test_pack_unpack_LE64);
+   SUITE_ADD_TEST(suite, test_unpack_little_endian_using_macros);
 
    return suite;
 }
@@ -95,3 +97,49 @@ static void test_pack_unpack_LE64(CuTest* tc)
    CuAssertULIntEquals(tc, value, result);
 }
 
+static void test_unpack_little_endian_using_macros(CuTest* tc)
+{
+#ifdef USE_PLATFORM_TYPES
+   uint8 buf[8];
+   uint8 u8Value;
+   uint16 u16Value;
+   uint32 u32Value;
+# if defined(__GNUC__) && defined(__LP64__)
+   uint64 u64Value;
+# endif
+#else
+   uint8_t buf[8];
+   uint8_t u8Value;
+   uint16_t u16Value;
+   uint32_t u32Value;
+# if defined(__GNUC__) && defined(__LP64__)
+   uint64_t u64Value;
+# endif
+#endif
+   uint32_t i;
+   const uint8_t *p;
+   const uint8_t *pBegin = &buf[0];
+   for (i=0; i<8; i++)
+   {
+      buf[i] = i;
+   }
+   p = pBegin;
+   u8Value = unpackU8(p);
+   CuAssertConstPtrEquals(tc, pBegin+1, p);
+   CuAssertUIntEquals(tc, 0u, u8Value);
+   p = pBegin;
+   u16Value = unpackU16LE(p);
+   CuAssertConstPtrEquals(tc, pBegin+2, p);
+   CuAssertUIntEquals(tc, 1*256u, u16Value);
+   p = pBegin;
+   u32Value = unpackU32LE(p);
+   CuAssertConstPtrEquals(tc, pBegin+4, p);
+   CuAssertUIntEquals(tc, 1*256u+2*65536u+3*16777216u, u32Value);
+#if defined(__GNUC__) && defined(__LP64__)
+   p = pBegin;
+   u64Value = unpackU64LE(p);
+   CuAssertConstPtrEquals(tc, pBegin+8, p);
+   CuAssertULIntEquals(tc, 1*256u+2*65536u+3*16777216u+4*4294967296UL+5*1099511627776UL+6*281474976710656UL+7*72057594037927936UL, u64Value);
+#endif
+
+}
