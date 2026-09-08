dd450c74562d4f9ced32dafb99cfcafe01904235
cogu <congus8@gmail.com>
2026-09-08 22:51:51 +0200

    Modernize filestream module and add unit tests

--- include/filestream.h
+++ include/filestream.h
@@ -1,35 +1,152 @@
-#ifndef FILESTREAM_H
-#define FILESTREAM_H
+/*****************************************************************************
+* \file      filestream.h
+* \author    Conny Gustafsson
+* \date      2015-02-14
+* \brief     File stream reader
+*
+* Copyright (c) 2015-2026 Conny Gustafsson
+* SPDX-License-Identifier: MIT
+* See LICENSE in project root for full license terms.
+******************************************************************************/
+#ifndef CUTIL_FILESTREAM_H_
+#define CUTIL_FILESTREAM_H_
+
+//////////////////////////////////////////////////////////////////////////////
+// INCLUDES
+//////////////////////////////////////////////////////////////////////////////
 #include <stdint.h>
 #include <stdio.h>
 #include "adt_bytearray.h"
 
-typedef struct ifstream_handler_t{
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//////////////////////////////////////////////////////////////////////////////
+// PUBLIC CONSTANTS AND DATA TYPES
+//////////////////////////////////////////////////////////////////////////////
+typedef struct cutil_ifstream_handler_tag {
    void *arg;
    void (*open)(void *arg);
    void (*close)(void *arg);
-   void (*write)(void *arg,const uint8_t *pChunk, uint32_t chunkLen);
-}ifstream_handler_t;
+   void (*write)(void *arg, const uint8_t *chunk, uint32_t chunk_len);
+} cutil_ifstream_handler_t;
 
-typedef struct ifstream_t{
-   ifstream_handler_t handler;
-}ifstream_t;
+typedef struct cutil_ifstream_tag {
+   cutil_ifstream_handler_t handler;
+} cutil_ifstream_t;
 
+// Compatibility aliases
+typedef cutil_ifstream_handler_t ifstream_handler_t;
+typedef cutil_ifstream_t ifstream_t;
 
-/***************** Public Function Declarations *******************/
-void ifstream_create(ifstream_t *self, ifstream_handler_t *handler);
-void ifstream_destroy(ifstream_t *self);
-ifstream_t *ifstream_new(ifstream_handler_t *handler);
-void ifstream_delete(ifstream_t *self);
+//////////////////////////////////////////////////////////////////////////////
+// PUBLIC FUNCTION PROTOTYPES
+//////////////////////////////////////////////////////////////////////////////
 
-void ifstream_open(ifstream_t *self);
-void ifstream_write(ifstream_t *self, const uint8_t *pChunk, uint32_t chunkLen);
-void ifstream_close(ifstream_t *self);
-int ifstream_readBinaryFile(ifstream_t *self,const char *filename);
-int ifstream_readTextFile(ifstream_t *self,const char *filename);
-int ifstream_readTextFileFromHandle(ifstream_t *self, FILE *fh);
+/**
+ * \brief Initializes a filestream instance in-place with a handler.
+ *
+ * \param self Pointer to filestream instance.
+ * \param handler Pointer to handler containing callbacks.
+ */
+void cutil_ifstream_create(cutil_ifstream_t *self, const cutil_ifstream_handler_t *handler);
 
-adt_bytearray_t *ifstream_util_readTextFile(const char *filename);
+/**
+ * \brief Destroys an in-place initialized filestream instance.
+ *
+ * \param self Pointer to filestream instance.
+ */
+void cutil_ifstream_destroy(cutil_ifstream_t *self);
 
+/**
+ * \brief Allocates and initializes a new filestream instance on heap.
+ *
+ * \param handler Pointer to handler containing callbacks.
+ * \return Newly allocated filestream instance, or NULL on error.
+ */
+cutil_ifstream_t *cutil_ifstream_new(const cutil_ifstream_handler_t *handler);
 
-#endif //FILESTREAM_H
+/**
+ * \brief Destroys and frees a heap-allocated filestream instance.
+ *
+ * \param self Pointer to filestream instance.
+ */
+void cutil_ifstream_delete(cutil_ifstream_t *self);
+
+/**
+ * \brief Invokes the open callback on the filestream handler.
+ *
+ * \param self Pointer to filestream instance.
+ */
+void cutil_ifstream_open(cutil_ifstream_t *self);
+
+/**
+ * \brief Invokes the write callback on the filestream handler with a data chunk.
+ *
+ * \param self Pointer to filestream instance.
+ * \param chunk Data buffer pointer.
+ * \param chunk_len Number of bytes in chunk.
+ */
+void cutil_ifstream_write(cutil_ifstream_t *self, const uint8_t *chunk, uint32_t chunk_len);
+
+/**
+ * \brief Invokes the close callback on the filestream handler.
+ *
+ * \param self Pointer to filestream instance.
+ */
+void cutil_ifstream_close(cutil_ifstream_t *self);
+
+/**
+ * \brief Reads a binary file in blocks and dispatches them to handler callbacks.
+ *
+ * \param self Pointer to filestream instance.
+ * \param filename Path to file to read.
+ * \return 0 on success, or -1 on error.
+ */
+int cutil_ifstream_read_binary_file(cutil_ifstream_t *self, const char *filename);
+
+/**
+ * \brief Reads a text file, normalizes CRLF line endings, and dispatches to handler callbacks.
+ *
+ * \param self Pointer to filestream instance.
+ * \param filename Path to file to read.
+ * \return 0 on success, or -1 on error.
+ */
+int cutil_ifstream_read_text_file(cutil_ifstream_t *self, const char *filename);
+
+/**
+ * \brief Reads a text file from an open FILE handle and dispatches to handler callbacks.
+ *
+ * \param self Pointer to filestream instance.
+ * \param fh Open FILE handle.
+ * \return 0 on success, or -1 on error.
+ */
+int cutil_ifstream_read_text_file_from_handle(cutil_ifstream_t *self, FILE *fh);
+
+/**
+ * \brief Convenience utility to read an entire text file into a newly created adt_bytearray_t.
+ *
+ * \param filename Path to file to read.
+ * \return Pointer to newly allocated adt_bytearray_t with file contents, or NULL on error.
+ */
+adt_bytearray_t *cutil_ifstream_util_read_text_file(const char *filename);
+
+// Legacy macro aliases
+#define ifstream_create cutil_ifstream_create
+#define ifstream_destroy cutil_ifstream_destroy
+#define ifstream_new cutil_ifstream_new
+#define ifstream_delete cutil_ifstream_delete
+#define ifstream_open cutil_ifstream_open
+#define ifstream_write cutil_ifstream_write
+#define ifstream_close cutil_ifstream_close
+#define ifstream_readBinaryFile cutil_ifstream_read_binary_file
+#define ifstream_readTextFile cutil_ifstream_read_text_file
+#define ifstream_readTextFileFromHandle cutil_ifstream_read_text_file_from_handle
+#define ifstream_util_readTextFile cutil_ifstream_util_read_text_file
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // CUTIL_FILESTREAM_H_
--- src/filestream.c
+++ src/filestream.c
@@ -1,5 +1,18 @@
+/*****************************************************************************
+* \file      filestream.c
+* \author    Conny Gustafsson
+* \date      2015-02-14
+* \brief     File stream reader
+*
+* Copyright (c) 2015-2026 Conny Gustafsson
+* SPDX-License-Identifier: MIT
+* See LICENSE in project root for full license terms.
+******************************************************************************/
+//////////////////////////////////////////////////////////////////////////////
+// INCLUDES
+//////////////////////////////////////////////////////////////////////////////
 #include <errno.h>
-#include <malloc.h>
+#include <stdlib.h>
 #include <assert.h>
 #include <string.h>
 #include <stdio.h>
@@ -8,115 +21,151 @@
 #include "CMemLeak.h"
 #endif
 
-
-
+//////////////////////////////////////////////////////////////////////////////
+// PRIVATE CONSTANTS AND DATA TYPES
+//////////////////////////////////////////////////////////////////////////////
 #define IFSTREAM_BLOCK_SIZE 8192
 
-/**************** Private Function Declarations *******************/
-static void util_appendChunk(void *arg, const uint8_t *pChunk, uint32_t chunkLen);
+//////////////////////////////////////////////////////////////////////////////
+// PRIVATE FUNCTION PROTOTYPES
+//////////////////////////////////////////////////////////////////////////////
+static void util_append_chunk(void *arg, const uint8_t *chunk, uint32_t chunk_len);
 
-/**************** Private Variable Declarations *******************/
+//////////////////////////////////////////////////////////////////////////////
+// PRIVATE VARIABLES
+//////////////////////////////////////////////////////////////////////////////
 
+//////////////////////////////////////////////////////////////////////////////
+// PUBLIC FUNCTIONS
+//////////////////////////////////////////////////////////////////////////////
 
-/****************** Public Function Definitions *******************/
-void ifstream_create(ifstream_t *self, ifstream_handler_t *handler){
-   if(self != 0){
-      memcpy(&self->handler,handler,sizeof(ifstream_handler_t));
+void cutil_ifstream_create(cutil_ifstream_t *self, const cutil_ifstream_handler_t *handler)
+{
+   if (self != NULL && handler != NULL)
+   {
+      memcpy(&self->handler, handler, sizeof(cutil_ifstream_handler_t));
    }
 }
 
-void ifstream_destroy(ifstream_t *self){
+void cutil_ifstream_destroy(cutil_ifstream_t *self)
+{
    (void) self;
 }
 
-ifstream_t *ifstream_new(ifstream_handler_t *handler){
-   ifstream_t *self = (ifstream_t*) malloc(sizeof(ifstream_t));
-   if(self != 0){
-      ifstream_create(self,handler);
+cutil_ifstream_t *cutil_ifstream_new(const cutil_ifstream_handler_t *handler)
+{
+   cutil_ifstream_t *self = (cutil_ifstream_t*) malloc(sizeof(cutil_ifstream_t));
+   if (self != NULL)
+   {
+      cutil_ifstream_create(self, handler);
    }
-   else{
+   else
+   {
       errno = ENOMEM;
    }
    return self;
 }
 
-void ifstream_delete(ifstream_t *self){
-   if(self != 0){
-      ifstream_destroy(self);
+void cutil_ifstream_delete(cutil_ifstream_t *self)
+{
+   if (self != NULL)
+   {
+      cutil_ifstream_destroy(self);
       free(self);
    }
 }
 
-void ifstream_open(ifstream_t *self){
-   if( (self != 0) && (self->handler.open != 0)){
+void cutil_ifstream_open(cutil_ifstream_t *self)
+{
+   if ((self != NULL) && (self->handler.open != NULL))
+   {
       self->handler.open(self->handler.arg);
    }
 }
 
-void ifstream_write(ifstream_t *self, const uint8_t *pChunk, uint32_t chunkLen){
-   if( (self != 0) && (self->handler.write != 0)){
-      self->handler.write(self->handler.arg,pChunk,chunkLen);
+void cutil_ifstream_write(cutil_ifstream_t *self, const uint8_t *chunk, uint32_t chunk_len)
+{
+   if ((self != NULL) && (self->handler.write != NULL))
+   {
+      self->handler.write(self->handler.arg, chunk, chunk_len);
    }
 }
 
-void ifstream_close(ifstream_t *self){
-   if( (self != 0) && (self->handler.close != 0)){
+void cutil_ifstream_close(cutil_ifstream_t *self)
+{
+   if ((self != NULL) && (self->handler.close != NULL))
+   {
       self->handler.close(self->handler.arg);
    }
 }
 
-int ifstream_readBinaryFile(ifstream_t *self,const char *filename)
+int cutil_ifstream_read_binary_file(cutil_ifstream_t *self, const char *filename)
 {
-   if( (self != 0) && (filename != 0) ){
-      uint32_t chunkLen = 0;
-      char *chunk;
-      chunk = (char*) malloc(IFSTREAM_BLOCK_SIZE);
-      if( chunk != 0 ){
-         FILE* fh = fopen(filename,"r");
-         if (fh != 0){
-            if(self->handler.open != 0){
+   if ((self != NULL) && (filename != NULL))
+   {
+      uint32_t chunk_len = 0;
+      char *chunk = (char*) malloc(IFSTREAM_BLOCK_SIZE);
+      if (chunk != NULL)
+      {
+         FILE* fh = fopen(filename, "rb");
+         if (fh != NULL)
+         {
+            if (self->handler.open != NULL)
+            {
                self->handler.open(self->handler.arg);
             }
-            while(1){
+            while (1)
+            {
                size_t len = fread(&chunk[0], 1, IFSTREAM_BLOCK_SIZE, fh);
                if (len > 0)
                {
-                  chunkLen = (uint32_t) len;
-                  if(self->handler.write != 0){
-                     self->handler.write(self->handler.arg,(uint8_t*)chunk, chunkLen);
+                  chunk_len = (uint32_t) len;
+                  if (self->handler.write != NULL)
+                  {
+                     self->handler.write(self->handler.arg, (uint8_t*) chunk, chunk_len);
                   }
                   if (len < IFSTREAM_BLOCK_SIZE)
                   {
                      break; //done
                   }
                }
+               else
+               {
+                  break;
+               }
             }
-            if(self->handler.close != 0){
+            if (self->handler.close != NULL)
+            {
                self->handler.close(self->handler.arg);
             }
-            if (chunk != 0) free(chunk);
+            free(chunk);
             fclose(fh);
             return 0;
          }
+         free(chunk);
       }
-      if (chunk != 0) free(chunk);
    }
-   else{
+   else
+   {
       errno = EINVAL;
    }
    return -1;
 }
 
-int ifstream_readTextFile(ifstream_t *self,const char *filename)
+int cutil_ifstream_read_text_file(cutil_ifstream_t *self, const char *filename)
 {
    int retval = 0;
-   if( (self != 0) && (filename != 0) )
+   if ((self != NULL) && (filename != NULL))
    {
-      FILE* fh = fopen(filename,"r");
-      if (fh != 0)
+      FILE* fh = fopen(filename, "r");
+      if (fh != NULL)
       {
-         retval =  ifstream_readTextFileFromHandle(self, fh);
+         retval = cutil_ifstream_read_text_file_from_handle(self, fh);
          fclose(fh);
+      }
+      else
+      {
+         retval = -1;
       }
    }
    else
@@ -127,58 +176,57 @@ int ifstream_readTextFile(ifstream_t *self,const char *filename)
    return retval;
 }
 
-int ifstream_readTextFileFromHandle(ifstream_t *self, FILE *fh)
+int cutil_ifstream_read_text_file_from_handle(cutil_ifstream_t *self, FILE *fh)
 {
    int retval = 0;
-   if( (self != 0) && (fh != 0) ){
-      uint32_t chunkLen = 0;
-      char *buf;
-      char *chunk;
-      buf = (char*) malloc(IFSTREAM_BLOCK_SIZE);
-      chunk = (char*) malloc(IFSTREAM_BLOCK_SIZE);
-      if( (buf != 0) && (chunk != 0) )
+   if ((self != NULL) && (fh != NULL))
+   {
+      uint32_t chunk_len = 0;
+      char *buf = (char*) malloc(IFSTREAM_BLOCK_SIZE);
+      uint8_t *chunk = (uint8_t*) malloc(IFSTREAM_BLOCK_SIZE);
+      if ((buf != NULL) && (chunk != NULL))
       {
-         if(self->handler.open != 0)
+         if (self->handler.open != NULL)
          {
             self->handler.open(self->handler.arg);
          }
 
-         while(fgets(buf, IFSTREAM_BLOCK_SIZE, fh) != NULL)
+         while (fgets(buf, IFSTREAM_BLOCK_SIZE, fh) != NULL)
          {
             size_t len = strlen(buf);
-            assert(len>0);
+            assert(len > 0);
             //dos2unix file ending
-            if( (len>=2) && buf[len-2] == '\r')
+            if ((len >= 2) && buf[len - 2] == '\r')
             {
                len--;
-               buf[len-1]='\n';
+               buf[len - 1] = '\n';
             }
-            if(len+chunkLen < IFSTREAM_BLOCK_SIZE)
+            if (len + chunk_len < IFSTREAM_BLOCK_SIZE)
             {
-               memcpy(&chunk[chunkLen],buf,len);
-               chunkLen += (uint32_t) len;
+               memcpy(&chunk[chunk_len], buf, len); // NOLINT(bugprone-not-null-terminated-result)
+               chunk_len += (uint32_t) len;
             }
             else
             {
-               if(self->handler.write != 0)
+               if (self->handler.write != NULL)
                {
-                  self->handler.write(self->handler.arg,(uint8_t*)chunk,chunkLen);
+                  self->handler.write(self->handler.arg, chunk, chunk_len);
                }
-               memcpy(&chunk[0],buf,len);
-               chunkLen = (uint32_t) len;
+               memcpy(&chunk[0], buf, len); // NOLINT(bugprone-not-null-terminated-result)
+               chunk_len = (uint32_t) len;
             }
          }
-         if( (chunkLen > 0) && (self->handler.write != 0))
+         if ((chunk_len > 0) && (self->handler.write != NULL))
          {
-            self->handler.write(self->handler.arg,(uint8_t*)chunk,chunkLen);
+            self->handler.write(self->handler.arg, chunk, chunk_len);
          }
-         if(self->handler.close != 0)
+         if (self->handler.close != NULL)
          {
             self->handler.close(self->handler.arg);
          }
       }
-      if (buf != 0) free(buf);
-      if (chunk != 0) free(chunk);
+      if (buf != NULL) free(buf);
+      if (chunk != NULL) free(chunk);
    }
    else
    {
@@ -188,34 +236,36 @@ int ifstream_readTextFileFromHandle(ifstream_t *self, FILE *fh)
    return retval;
 }
 
-adt_bytearray_t *ifstream_util_readTextFile(const char *filename)
+adt_bytearray_t *cutil_ifstream_util_read_text_file(const char *filename)
 {
    adt_bytearray_t *buf = adt_bytearray_new();
-   if (buf != 0)
+   if (buf != NULL)
    {
-      ifstream_handler_t handler;
-      ifstream_t ifstream;
+      cutil_ifstream_handler_t handler;
+      cutil_ifstream_t ifstream;
       memset(&handler, 0, sizeof(handler));
       handler.arg = buf;
-      handler.write = util_appendChunk;
-      ifstream_create(&ifstream, &handler);
-      if (ifstream_readTextFile(&ifstream, filename) != 0)
+      handler.write = util_append_chunk;
+      cutil_ifstream_create(&ifstream, &handler);
+      if (cutil_ifstream_read_text_file(&ifstream, filename) != 0)
       {
          adt_bytearray_delete(buf);
-         buf = (adt_bytearray_t*) 0;
+         buf = NULL;
       }
       return buf;
    }
-   return (adt_bytearray_t*) 0;
+   return NULL;
 }
 
+//////////////////////////////////////////////////////////////////////////////
+// PRIVATE FUNCTIONS
+//////////////////////////////////////////////////////////////////////////////
 
-/***************** Private Function Definitions *******************/
-static void util_appendChunk(void *arg, const uint8_t *pChunk, uint32_t chunkLen)
+static void util_append_chunk(void *arg, const uint8_t *chunk, uint32_t chunk_len)
 {
    adt_bytearray_t *buf = (adt_bytearray_t*) arg;
-   if (buf != 0 && pChunk != 0 && chunkLen > 0)
+   if (buf != NULL && chunk != NULL && chunk_len > 0)
    {
-      adt_bytearray_append(buf, pChunk, chunkLen);
+      adt_bytearray_append(buf, chunk, chunk_len);
    }
 }
