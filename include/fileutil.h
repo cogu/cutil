70d6f631bbc86dfce29a20b4086fdc2be6dcbeb7
cogu <congus8@gmail.com>
2026-09-08 22:51:43 +0200

    Modernize fileutil module and unit tests

--- include/fileutil.h
+++ include/fileutil.h
@@ -5,26 +5,11 @@
 * \brief     Cross-platform file and path utilities
 *
 * Copyright (c) 2026 Conny Gustafsson
-* Permission is hereby granted, free of charge, to any person obtaining a copy of
-* this software and associated documentation files (the "Software"), to deal in
-* the Software without restriction, including without limitation the rights to
-* use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-* the Software, and to permit persons to whom the Software is furnished to do so,
-* subject to the following conditions:
-
-* The above copyright notice and this permission notice shall be included in all
-* copies or substantial portions of the Software.
-
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-* FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-* COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-* IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-* CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*
+* SPDX-License-Identifier: MIT
+* See LICENSE in project root for full license terms.
 ******************************************************************************/
-#ifndef FILEUTIL_H
-#define FILEUTIL_H
+#ifndef CUTIL_FILEUTIL_H_
+#define CUTIL_FILEUTIL_H_
 
 //////////////////////////////////////////////////////////////////////////////
 // INCLUDES
@@ -32,10 +17,39 @@
 #include <stdbool.h>
 #include "adt_str.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//////////////////////////////////////////////////////////////////////////////
+// PUBLIC CONSTANTS AND DATA TYPES
+//////////////////////////////////////////////////////////////////////////////
+
 //////////////////////////////////////////////////////////////////////////////
 // PUBLIC FUNCTION PROTOTYPES
 //////////////////////////////////////////////////////////////////////////////
+
+/**
+ * \brief Checks if the given path is an existing directory.
+ *
+ * \param path File system path to check.
+ * \return true if path exists and is a directory, false otherwise.
+ */
 bool cutil_is_dir(const char *path);
+
+/**
+ * \brief Joins a directory path and a filename into a newly allocated adt_str_t.
+ *
+ * Handles directory separator concatenation cleanly without duplicating slashes.
+ *
+ * \param dir Directory path.
+ * \param filename File name or sub-path.
+ * \return Pointer to newly allocated adt_str_t, or NULL on memory allocation failure.
+ */
 adt_str_t *cutil_path_join(const char *dir, const char *filename);
 
-#endif // FILEUTIL_H
+#ifdef __cplusplus
+}
+#endif
+
+#endif // CUTIL_FILEUTIL_H_
